547db02fb82041ae71f403639542da44a0ec069a
Andrew Joslin <andrewtjoslin@gmail.com>
2016-04-23 09:59:51 -0700

    Don't lint, it was breaking murmurhash

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ var test = require('tape')
 var hash = require('./')
 
 test('sanity test', function (t) {
-  t.equal(hash('it should hash this'), 369077401)
+  t.equal(hash('it should hash this'), 3699533434)
   t.end()
 })
